fe1236b56477c40c60e1b9f2abdfd0c3c6a5fba0
oskal <artem-osipovartem3002@mail.ru>
2024-11-12 00:01:26 +0300

    feat: update applyFormat

--- src/components/email-editor/EmailEditor.tsx
+++ src/components/email-editor/EmailEditor.tsx
@@ -1,32 +1,59 @@
 import { Bold, Eraser, Italic, Underline } from 'lucide-react';
 import styles from './EmailEditor.module.scss';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import { applyStyle, TStyle } from './apply-style';
+import parse from 'html-react-parser';
 
 export function EmailEditor() {
-  const [text, useText] =
+  const [text, setText] =
     useState(`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti omnis consequuntur,
           adipisci commodi placeat praesentium molestiae totam neque illum labore incidunt
           exercitationem, eaque nesciunt laboriosam consequatur iste. Facere, dolorum amet!`);
 
+  const [selectionStart, setSelectionStart] = useState(0);
+  const [selectionEnd, setSelectionEnd] = useState(0);
+
+  const textRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const updateSelection = () => {
+    if (!textRef.current) return;
+    setSelectionStart(textRef.current?.selectionStart);
+    setSelectionEnd(textRef.current?.selectionEnd);
+  };
+
+  const applyFormat = (type: TStyle) => {
+    const selectedText = text.substring(selectionStart, selectionEnd);
+    if (!selectedText) return;
+    const before = text.substring(0, selectionStart);
+    const after = text.substring(selectionEnd);
+    setText(before + applyStyle(type, selectedText) + after);
+  };
+
   return (
     <div>
       <h1>Email editor</h1>
+      <div className={styles.preview}>{parse(text)}</div>
       <div className={styles.card}>
-        <div contentEditable className={styles.editor} spellCheck='false'>
-          {text}
-        </div>
+        <textarea
+          ref={textRef}
+          onSelect={updateSelection}
+          className={styles.editor}
+          spellCheck='false'
+          value={text}
+          onChange={e => setText(e.target.value)}
+        />
         <div className={styles.actions}>
           <div className={styles.tools}>
-            <button>
+            <button onClick={() => setText('')}>
               <Eraser size={17} />
             </button>
-            <button>
+            <button onClick={() => applyFormat('bold')}>
               <Bold size={17} />
             </button>
-            <button>
+            <button onClick={() => applyFormat('italic')}>
               <Italic size={17} />
             </button>
-            <button>
+            <button onClick={() => applyFormat('underline')}>
               <Underline size={17} />
             </button>
           </div>
